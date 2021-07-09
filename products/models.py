2d0e14be2f1507447a25f07df6ea0b199f506f3a
elomasney <eimearlomasney01@gmail.com>
2021-07-09 18:14:01 +0000

    Added fixtures for products models and added productfeature model

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -17,11 +21,20 @@ class Product(models.Model):
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    features = models.JSONField()
-    online_price = models.DecimalField(max_digits=6, decimal_places=2)
-    person_price = models.DecimalField(max_digits=6, decimal_places=2)
+    online_price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductFeature(models.Model):
+    name = models.CharField(max_length=254)
+    product = models.ManyToManyField(
+        'Product', on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
