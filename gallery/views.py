d89d7c79935bb14a1a986baa695683f10f6cd7ea
elomasney <eimearlomasney01@gmail.com>
2021-07-14 11:17:08 +0000

    Added navbar filter gallery by category

--- gallery/views.py
+++ gallery/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from .models import Image
+from .models import Image, Collection
 
 
 # Create your views here.
 def gallery(request):
     """ A view to return the Gallery Page"""
     images = Image.objects.all()
+    collections = None
+
+    if request.GET:
+        if 'collection' in request.GET:
+            collections = request.GET['collection'].split(',')
+            images = images.filter(collection__name__in=collections)
+            collections = Collection.objects.filter(name__in=collections)
 
     context = {
         'images': images,
+        'current_collections': collections
     }
     return render(request, 'gallery/gallery.html', context)
