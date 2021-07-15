be45316ee09110d47b3ce20bf0244f1233b52a20
elomasney <eimearlomasney01@gmail.com>
2021-07-15 15:37:02 +0000

    Updated shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,9 +9,9 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity, price, in bag.items():
+    for item_id, quantity, in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += price
+        total += quantity * product.price
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
