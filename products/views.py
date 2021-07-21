068e270b0cf84a935efca4b6b17f95ddd5513632
elomasney <eimearlomasney01@gmail.com>
2021-07-21 17:07:44 +0000

    Add product view, url, template

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
 from django.db.models import Q
 from .models import Product, ProductFeature, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -51,3 +53,16 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+@staff_member_required
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
