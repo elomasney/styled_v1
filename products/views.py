cf24b3cd4f642ffbd03d34a77cb881cc8105dd01
elomasney <eimearlomasney01@gmail.com>
2021-07-22 11:12:13 +0000

    Added product delete functionality

--- products/views.py
+++ products/views.py
@@ -80,6 +80,9 @@ def add_product(request):
 @staff_member_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
@@ -101,3 +104,16 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@staff_member_required
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
