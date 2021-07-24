cec611c378aad7f9def3c46a4b3b1c1e0437cb09
elomasney <eimearlomasney01@gmail.com>
2021-07-24 13:26:03 +0000

    Resolved email settings issue

--- styled/settings.py
+++ styled/settings.py
@@ -117,7 +117,7 @@ else:
     EMAIL_HOST = os.getenv('EMAIL_HOST')
     EMAIL_PORT = os.getenv('EMAIL_PORT')
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_PASSWORD')
     EMAIL_USE_TLS = True
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
