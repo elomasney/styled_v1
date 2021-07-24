76bb1181a2f59fe9f8762a348872d2498e477b7c
elomasney <eimearlomasney01@gmail.com>
2021-07-24 20:23:20 +0000

    Testing Sendgrid email functionality

--- styled/settings.py
+++ styled/settings.py
@@ -110,11 +110,11 @@ SITE_ID = 1
 
 if 'HEROKU_ON' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = os.environ.get('EMAIL_HOST')
-    EMAIL_PORT = os.environ.get('EMAIL_PORT')
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST = os.getenv('EMAIL_HOST')
+    EMAIL_PORT = os.getenv('EMAIL_PORT')
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
     EMAIL_USE_TLS = True
 else:
     DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
