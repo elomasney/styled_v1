487776056a0c19d3c705fb0cd4a8da4e1b419c46
elomasney <eimearlomasney01@gmail.com>
2021-07-24 07:44:07 +0000

    Updated links for mobile navbar

--- styled/settings.py
+++ styled/settings.py
@@ -135,11 +135,11 @@ if 'DATABASE_URL' in os.environ:
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
         }
-    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
